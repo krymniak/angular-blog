f98476e9245208b46f1774ac3940b1b6f00c06e1
krymniak <krymnyak@gmail.com>
2022-08-12 12:26:50 +0300

    admin layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,17 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { PostPageComponent } from './post-page/post-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainLayoutComponent,
-    HomePageComponent,
-    PostPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		MainLayoutComponent,
+		HomePageComponent,
+		PostPageComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
