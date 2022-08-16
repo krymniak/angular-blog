b56690240b9d8ee8c27387e62a12ecd9ae0e1d6a
krymniak <krymnyak@gmail.com>
2022-08-16 10:43:02 +0300

    guarding routes

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { User } from 'src/app/shared/components/interfaces';
 import { AuthService } from '../shared/components/admin-layout/services/auth.service';
 
@@ -12,13 +12,20 @@ import { AuthService } from '../shared/components/admin-layout/services/auth.ser
 export class LoginPageComponent implements OnInit {
 	form!: FormGroup
 	submitted = false
+	message!: string
 
 	constructor(
 		public auth: AuthService,
-		private router: Router
+		private router: Router,
+		private route: ActivatedRoute
 	) { }
 
 	ngOnInit(): void {
+		this.route.queryParams.subscribe((params: Params) => {
+			if (params['loginAgain']) {
+				this.message = 'Please enter value'
+			}
+		})
 		this.form = new FormGroup({
 			email: new FormControl(null, [
 				Validators.required,
