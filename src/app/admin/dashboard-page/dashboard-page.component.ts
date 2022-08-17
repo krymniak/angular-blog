bdb53453f48b6c28217f5387981cc48ab198e879
krymniak <krymnyak@gmail.com>
2022-08-17 11:01:50 +0300

    output posts list

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from 'src/app/shared/components/interfaces';
+import { PostService } from 'src/app/shared/post.service';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
+posts: Post[] = []
+pSub!: Subscription
 
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+		this.pSub = this.postService.getAll().subscribe(posts => {
+			this.posts = posts
+		})
   }
+
+	remove(id: string) {
+		
+	}
+
+	ngOnDestroy(): void {
+		if(this.pSub) {
+			this.pSub.unsubscribe()
+		}
+	}
 
 }
